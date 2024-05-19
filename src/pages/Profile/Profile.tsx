61c1f0284a72d8648e924d2a66d9333301153010
top0329 <top000329@gmail.com>
2024-05-20 02:27:22 +0900

    uiUpdated

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -4,10 +4,10 @@ import ProfileCard from './ProfileCard';
 
 
 const Profile: React.FC = () => {
- 
+
   return (
     <DefaultLayout>
-      <div className="relative flex py-2 items-center justify-center px-[30px] md:px-[60px] w-full min-h-[600px] md:min-h-[800px] box-border rounded-[26px] border-[1px] border-dashed" style={{ borderColor: '#4D8CEC', flex: "25%" }}>
+      <div className="relative flex py-2 items-center justify-center px-0 md:px-[60px] w-full min-h-[600px] md:min-h-[800px] border-none box-border rounded-[26px] border-[1px] md:border-dashed" style={{ borderColor: '#4D8CEC', flex: "25%" }}>
         <img src="./cornerLB.svg" alt="img" className='absolute md:bottom-0 bottom-[-80px] left-0 h-[50%] w-[100%] md:w-[50%] z-0' />
         <img src="./cornerRT.svg" alt="img" className='absolute top-0 right-0 h-[50%] w-[50%] z-0' />
         <ProfileCard />
--- src/pages/Profile/ProfileCard.tsx
+++ src/pages/Profile/ProfileCard.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import useScreenSize from '../../hooks/useScreenSize';
+
 const nodesData = [
     {
         id: 1,
         name: "Node One",
-        specification: "High Performance Computing",
+        specification: "NVIDIA H100",
         type: "GPU",
         region: "USA East",
         status: "Active"
@@ -27,47 +29,87 @@ const nodesData = [
 ];
 const ProfileCard: React.FC = () => {
     const currentBalance = 200;
+    const screenSize = useScreenSize();
+    const isSmallScreen = screenSize.width <= 640; // Assuming 640px is the breakpoint for small screens
     return (
         <div className='z-0 flex h-full w-full items-center justify-start flex-col py-[40px] pt-[30px] md:pt-[70px]'>
             <section className='flex flex-col gap-10 w-full pb-[40px]'>
                 <h1 className='font-press-start-2p light-theme-color md:text-[20px] text-[18px]'>My Balance</h1>
-                <div className='flex flex-col w-[300px] px-6 py-4 font-space-grotesk gap-4 border-2 rounded-[10px]' style={{ borderColor: "#4D8CEC" }}>
+                <div className='flex flex-col md:text-left text-center md:w-[300px] w-[250px] px-6 py-4 font-space-grotesk gap-4 border-2 rounded-[10px]' style={{ borderColor: "#4D8CEC" }}>
                     <h2 className='dark-theme-color md:text-[20px] text-[18px]'>Current Balance</h2>
                     <h1 className='light-theme-color lg:text-[24px] xl:text-[36px] text-[21px]'>${currentBalance}</h1>
                 </div>
             </section>
             <section className='flex flex-col gap-10 w-full pb-[40px]'>
                 <h1 className='font-press-start-2p light-theme-color md:text-[20px] text-[18px]'>My Nodes</h1>
-                <div className="border border-dashed rounded-[20px] overflow-hidden" style={{ borderColor: "#4D8CEC" }}>
-                    <table className="w-full font-space-grotesk">
-                        <thead className='bg-light-theme-color text-white text-[18px] h-[60px]'>
-                            <tr>
-                                <th>Name</th>
-                                <th>Specification</th>
-                                <th>Type</th>
-                                <th>Region</th>
-                                <th>Status</th>
-                            </tr>
-                        </thead>
-                        <tbody className='text-center justify-center text-[15px] dark-theme-color'>
-                            {nodesData.length > 0 ? (
-                                nodesData.map(node => (
-                                    <tr key={node.id} className='h-[80px]'>
-                                        <td>{node.name}</td>
-                                        <td>{node.specification}</td>
-                                        <td>{node.type}</td>
-                                        <td>{node.region}</td>
-                                        <td>{node.status}</td>
-                                    </tr>
-                                ))
-                            ) : (
-                                <tr>
-                                    <td colSpan={5}>No nodes</td>
-                                </tr>
-                            )}
-                        </tbody>
-                    </table>
-                </div>
+                {
+                    isSmallScreen ?
+                        (
+                            nodesData.map((node, index) => (
+                                <div key={index} className='flex flex-col font-space-grotesk border border-dashed rounded-[10px]' style={{ borderColor: '#4D8CEC' }}>
+                                    <div className='flex flex-row py-2  px-2' >
+                                        <h1 className='dark-theme-color text-[18px] font-bold'>Name:&nbsp;</h1>
+                                        <h2 className='dark-theme-color text-[15px]'>{node.name}</h2>
+                                    </div>
+                                    <div className='flex flex-row py-2 px-2'>
+                                        <h1 className='dark-theme-color text-[18px] font-bold'>Specification:&nbsp;</h1>
+                                        <h2 className='dark-theme-color text-[15px]'>{node.specification}</h2>
+                                    </div>
+                                    <div className='flex flex-row py-2 px-2'>
+                                        <h1 className='dark-theme-color text-[18px] font-bold'>Type:&nbsp;</h1>
+                                        <h2 className='dark-theme-color text-[15px]'>{node.type}</h2>
+                                    </div>
+                                    <div className='flex flex-row py-2 px-2'>
+                                        <h1 className='dark-theme-color text-[18px] font-bold'>Region:&nbsp;</h1>
+                                        <div className='flex flex-row items-center justify-center gap-2'>
+                                            <img src={`https://flagcdn.com/w40/us.png`} alt="us" className={`rounded-full overflow-hidden w-8 h-8`} />
+                                            {node.region}
+                                        </div>
+                                    </div>
+                                    <div className='flex flex-row py-2 px-2'>
+                                        <h1 className='dark-theme-color text-[18px] font-bold'>Status:&nbsp;</h1>
+                                        <h2 className='dark-theme-color text-[15px]'>{node.status}</h2>
+                                    </div>
+                                </div>
+                            ))
+                        ) : (
+                            <div className="border border-dashed rounded-[20px] overflow-hidden" style={{ borderColor: "#4D8CEC" }}>
+                                <table className="w-full font-space-grotesk">
+                                    <thead className='bg-light-theme-color text-white text-[18px] h-[60px]'>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Specification</th>
+                                            <th>Type</th>
+                                            <th>Region</th>
+                                            <th>Status</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody className='text-center justify-center text-[15px] dark-theme-color'>
+                                        {nodesData.length > 0 ? (
+                                            nodesData.map(node => (
+                                                <tr key={node.id} className='h-[80px]'>
+                                                    <td>{node.name}</td>
+                                                    <td>{node.specification}</td>
+                                                    <td>{node.type}</td>
+                                                    <td>
+                                                        <div className='flex flex-row items-center justify-center gap-2'>
+                                                            <img src={`https://flagcdn.com/w40/us.png`} alt="us" className={`rounded-full overflow-hidden w-8 h-8`} />
+                                                            {node.region}
+                                                        </div>
+                                                    </td>
+                                                    <td>{node.status}</td>
+                                                </tr>
+                                            ))
+                                        ) : (
+                                            <tr>
+                                                <td colSpan={5}>No nodes</td>
+                                            </tr>
+                                        )}
+                                    </tbody>
+                                </table>
+                            </div>
+                        )
+                }
             </section>
         </div >
     );
